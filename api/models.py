895dfe5af3e7bf6e09dd6f5aecd81fffc12d949b
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-11 21:41:32 +0000

    working on category

--- api/models.py
+++ api/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='categories')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+    def __str__(self):
+        return self.name
 
 
 
--- api/serializer.py
+++ api/serializer.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Post, Comment, Category
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    owner= serializers.ReadOnlyField(source='owner.username')
+
+
+    class Meta:
+        model = Category
+        fields = ['id','name','owner']
 
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # categories = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Post
-        fields = ['id','title','body','owner','comments']
+        fields = ['id','title','body','owner','comments','categories']
 
 
 class UserSerializer(serializers.ModelSerializer):
