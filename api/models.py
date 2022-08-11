520a37dfddc51847004e156aee3f11027838cb5a
Robert Coffie <rcoffie22@yhahoo.com>
2022-08-11 21:09:36 +0000

    adding category model

--- api/models.py
+++ api/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='categories')
+
+
+
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     body = models.TextField(blank=True, default='')
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
     class Meta:
         ordering = ['created']
 
